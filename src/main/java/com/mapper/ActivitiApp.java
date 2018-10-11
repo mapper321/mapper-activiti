679552c0557624dfdb4deb1c3fbdaea37be8c322
mapper <547008022@qq.com>
2018-10-11 14:37:19 +0800

    引入activiti

--- src/main/java/com/mapper/ActivitiApp.java
+++ src/main/java/com/mapper/ActivitiApp.java
@@ -1,28 +1,18 @@
 package com.mapper;
 
-import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableTransactionManagement
 @Controller
-public class AvtivitiApp {
+public class ActivitiApp {
 	
 	
 	@RequestMapping("/")
@@ -33,7 +23,7 @@ public class AvtivitiApp {
 	
 	
 	public static void main(String[] args) {
-		SpringApplication.run(AvtivitiApp.class,args);
+		SpringApplication.run(ActivitiApp.class,args);
     }
 
 }
